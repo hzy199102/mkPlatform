1f4e058057f94b40bd59715ceb0ff605ba4ca4b1
huangzy <huangzy@glodon.com>
2017-11-22 21:09:41 +0800

    1.md目录生成彻底完成 2.优化内容滚动定位目录节点的算法 3.优化图片场景，当内容包含图片的时候，解决内容滚动定位目录节点不准确的问题

--- md/factory/two/js/md_handle.js
+++ md/factory/two/js/md_handle.js
@@ -13,7 +13,79 @@
 ;(function($) {
 
     /**
+     * 打印日志
+     * @param opts
+     * @param str
+     * @private
+     */
+    _log = function (opts, str) {
+        if (opts.debug) {
+            console.log(str)
+        }
+    }
+
+    /**
+     * 更新位置_header_offsets，注意是全部的，因为不知道img是哪个标题中的
+     * @param opts
+     * @private
+     */
+    _update_position_data = function (opts) {
+        opts._header_offsets = []
+        $('#' + opts._mdContentId).find(':header').each(function() {
+            opts._header_offsets.push($(this).offset().top);
+        });
+        _bind_scroll_event_and_update_postion(opts)
+    }
+
+    /**
+     * 图片加载有延时性，全部图片加载完毕之后更新位置_header_offsets，这里用不到，需要jquery版本 > 1.5
+     * @param opts
+     * @private
+     */
+    _all_img_handle = function (opts) {
+        var imgdefereds = [];
+        $('img').each(function(){
+            var dfd = $.Deferred();
+            $(this).bind('load',function(){
+                dfd.resolve();
+            }).bind('error',function(){
+                //图片加载错误，加入错误处理
+                dfd.resolve();
+            })
+            if(this.complete) setTimeout(function(){
+                dfd.resolve();
+            },1000);
+            imgdefereds.push(dfd);
+        })
+        $.when.apply(null,imgdefereds).done(function(){
+            _update_position_data(opts)
+        });
+    }
+
+    /**
+     * 图片加载有延时性，需要在每张图片加载完成之后更新位置_header_offsets
+     * @param opts
+     * @private
+     */
+    _img_handle = function (opts) {
+        $('img').each(function(){
+            // 如果图片已经存在于浏览器缓存，直接调用回调函数
+            if(this.complete) {
+                _update_position_data(opts)
+                return false;
+            }
+            $(this).bind('load',function(){
+                _update_position_data(opts)
+            }).bind('error',function(){
+                //图片加载错误，加入错误处理
+                _update_position_data(opts)
+            })
+        })
+    }
+
+    /**
      * 根据header生成ztree的node的id
+     * 根目录节点默认是0
      * @param headerLevel
      * @param headerStep
      * @param type id类型，false：自身id；true：父id;
@@ -23,17 +95,17 @@
     _ztree_node_id = function (headerLevel, headerStep, type) {
         var result = 0;
         var _type = type ? 1 : 0;
-        if (headerLevel.length === type) {
+        if (headerLevel.length === _type) {
             return result;
         }
-        for (var i = _type; i < headerLevel.length; i++ ) {
-            result += headerLevel[i] * Math.pow(headerStep, headerLevel.length - i - 1);
+        for (var i = 0; i < headerLevel.length - _type; i++ ) {
+            result += headerLevel[i] * Math.pow(headerStep, headerLevel.length - _type - i - 1);
         }
         return result;
     }
 
     /**
-     * 生成ztree的源数据，给header绑定id，绑定锚点
+     * 生成ztree的源数据，给header绑定id，绑定锚点，并且确定节点对应的内容位置
      * @param opts
      * @param header_obj
      * @private
@@ -43,6 +115,7 @@
         var pId = _ztree_node_id(opts._headerLevel, opts._headerStep, true);
         // 设置锚点id
         $(header_obj).attr('id', id);
+        opts._header_offsets.push($(header_obj).offset().top);
         opts._zTree_nodes.push({
             id: id,
             pId: pId ,
@@ -89,6 +162,7 @@
      */
     _md_header = function (opts) {
         // 选择所有标题元素（h1 - h6）
+        opts._header_offsets = []
         $('#' + opts._mdContentId).find(':header').each(function() {
             var level = parseInt(this.nodeName.substring(1), 10);
 
@@ -98,18 +172,72 @@
         });
     }
 
+    /*
+     * 根据滚动确定当前位置，并更新ztree，不考虑上限和下限问题，即如果最末尾已经超出ztree最后一个节点的覆盖范围，仍然最后一个节点高亮，首个节点类似
+     * @param opts
+     * @private
+     */
+    function _bind_scroll_event_and_update_postion(opts) {
+        var timeout;
+        var highlight_on_scroll = function(e) {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+
+            timeout = setTimeout(function() {
+                // 保持10px的偏移量，效果更好
+                var top = $(window).scrollTop() + 10;
+                var p = 0;
+                for (var i = 0, c = opts._header_offsets.length; i < c; i++) {
+                    if (opts._header_offsets[0] > top) {
+                        p = 0;
+                        break;
+                    }
+                    if (opts._header_offsets[opts._header_offsets.length - 1] < top) {
+                        p = opts._header_offsets.length - 1;
+                        break;
+                    }
+                    if (opts._header_offsets[i] > top && opts._header_offsets[i - 1] < top) {
+                        p = i - 1;
+                        break;
+                    }
+                }
+                // 当前内容位置
+                _log(opts, 'opts._header_offsets['+ p +'] = '+ opts._header_offsets[p]);
+                $('a').removeClass('curSelectedNode');
+                // 这里利用了ztree的节点id是从1到N这样的顺序下来的设定
+                var obj = $('#md_tree_' + (p + 1) + '_a').addClass('curSelectedNode');
+            }, opts._refresh_scroll_time);
+        };
+
+        if (opts._highlight_on_scroll) {
+            $(window).bind('scroll', highlight_on_scroll);
+            highlight_on_scroll();
+        }
+    }
+
     $.fn.md_handle = function(options) {
         // 将defaults 和 options 参数合并到{}
-        var opts = $.extend({},_defaults,options);
+        $.fn.md_handle.defaults = $.extend({}, $.fn.md_handle.defaults, options);
+
+        var opts = $.fn.md_handle.defaults
+
+        _img_handle(opts);
 
         _md_header(opts);
 
         // 初始化ztree
         $.fn.zTree.init($('#' + opts._zTreeId), opts.ztreeSetting, opts._zTree_nodes).expandAll(opts._ztree_expandAll);
+
+        _bind_scroll_event_and_update_postion(opts);
     }
 
     //定义默认
-    _defaults = {
+    $.fn.md_handle.defaults = {
+        /**
+         * 是否打印日志
+         */
+        debug: true,
         /**
          * ztree id
          */
@@ -136,9 +264,25 @@
          */
         _autoNumber: true,
         /**
-         * 默认折叠全部节点
+         * 默认展开全部节点
          */
-        _ztree_expandAll: false,
+        _ztree_expandAll: true,
+        /**
+         * 内容各级标题对应的位置
+         */
+        _header_offsets: [],
+        /**
+         * 滚动是否节点高亮
+         */
+        _highlight_on_scroll: true,
+        /*
+         * 计算滚动判断当前位置的时间，默认是50毫秒
+         */
+        _refresh_scroll_time: 50,
+        /**
+         * 选中节点时对应的内容标题有动画效果
+         */
+        _is_highlight_selected_line: true,
         /**
          * ztree配置，具体参考ztreeAPI
          */
@@ -154,27 +298,16 @@
                     enable: true,
                     idKey : "id",
                     pIdKey: "pId",
-                    // rootPId: "0"
+                    rootPId: null
                 }
             },
             callback: {
                 beforeClick: function(treeId, treeNode) {
                     $('a').removeClass('curSelectedNode');
-                    if(treeNode.id == 1){
-                        // TODO: when click root node
-                        console.log('click root table of content');
-                    }
-                    if($.fn.ztree_toc.defaults.is_highlight_selected_line == true) {
+                    if($.fn.md_handle.defaults._is_highlight_selected_line) {
                         $('#' + treeNode.id).css('color' ,'red').fadeOut("slow" ,function() {
-                            // Animation complete.
                             $(this).show().css('color','black');
                         });
-                    }
-                },
-                onRightClick: function(event, treeId, treeNode) {
-                    if(treeNode.id == 1){
-                        // TODO: when right_click root node:table content
-                        console.log('right_click root table of content');
                     }
                 }
             }
--- md/main/test2.md
+++ md/main/test2.md
@@ -1,19 +1,103 @@
+## hehheheh
 
+hehehhehee
 
 # 一级标题
 
 A
 
+B
+
+C
+
+D
+
+E
+
+F
+
+G
+
 ## aaa
 
-aaaa
+* 1
+* 2
+* 3
+
+1. 1
+2. 2
+3. 3
 
 #### 四级标题1
 四级标题
 #### 四级标题2
 四级标题2
-
-
+### 三级标题1
+三级标题1
+### 三级标题2
+三级标题2
 ## bbbbbb
 
+> 这是一段引用
 
+要注意符号和文本之间的空格
+
+## eee
+
+eeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeee
+
+##cccc
+
+| name               |       asd       |        zzz | asdasd |
+|:------------------ |:---------------:| ----------:|:------:|
+| sadaaaaaaaaaaaaaaa | asdsadsadsadsad | asdasddddd | 11111  |
+| 12                 |     sadasd      |         22 |   44   |
+
+
+***
+
+**强调**
+
+
+
+## ddd
+
+```JavaScript
+function $initHighlight(block, cls) {
+  try {
+    if (cls.search(/\bno\-highlight\b/) != -1)
+      return process(block, true, 0x0F) +
+             ` class="${cls}"`;
+  } catch (e) {
+    /* handle exception */
+  }
+  for (var i = 0 / 2; i < classes.length; i++) {
+    if (checkCondition(classes[i]) === undefined)
+      console.log('undefined');
+  }
+}
+
+export  $initHighlight;
+```
+
+```JSON
+[
+  {
+    "title": "apples",
+    "count": [12000, 20000],
+    "description": {"text": "...", "sensitive": false}
+  },
+  {
+    "title": "oranges",
+    "count": [17500, null],
+    "description": {"text": "...", "sensitive": false}
+  }
+]
+```
+## eeee
+这是最后
